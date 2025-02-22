37ee40788adb4b84461b1eced3658685133354c5
Max Martyanov <martyanovmax@mail.ru>
2025-02-22 20:49:06 +0300

    refactor(presentation): create presentation package and move MainActivity

--- app/src/main/java/max/keils/habitmate/presentation/MainActivity.kt
+++ app/src/main/java/max/keils/habitmate/presentation/MainActivity.kt
@@ -1,10 +1,11 @@
-package max.keils.habitmate
+package max.keils.habitmate.presentation
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import max.keils.habitmate.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
