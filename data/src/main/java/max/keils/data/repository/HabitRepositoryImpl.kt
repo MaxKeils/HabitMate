5b93117b3467c6dde3d0465885015fefab550273
Max Martyanov <martyanovmax@mail.ru>
2025-02-25 14:31:22 +0300

    chore(data): add habit repository implementation

---
+++ data/src/main/java/max/keils/data/repository/HabitRepositoryImpl.kt
@@ -0,0 +1,44 @@
+package max.keils.data.repository
+
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.update
+import max.keils.domain.model.Habit
+import max.keils.domain.repository.HabitRepository
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class HabitRepositoryImpl @Inject constructor() : HabitRepository {
+
+    private val _habits = MutableStateFlow(emptyList<Habit>())
+
+    override suspend fun addHabit(habit: Habit) {
+        _habits.update { it + habit }
+    }
+
+    override suspend fun deleteHabit(habitId: Int) {
+        _habits.update { habits ->
+            habits.filter { habit ->
+                habit.id != habitId
+            }
+        }
+    }
+
+    override suspend fun updateHabit(newHabit: Habit) {
+        _habits.update { habits ->
+            habits.map { habit ->
+                if (habit.id == newHabit.id)
+                    newHabit
+                else
+                    habit
+            }
+        }
+    }
+
+    override suspend fun getHabitById(habitId: Int): Habit? =
+        _habits.value.find { it.id == habitId }
+
+
+    override fun getHabits(): Flow<List<Habit>> = _habits
+}
