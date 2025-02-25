370ce807393d752ed0549016b483dcdf35e9fa65
Max Martyanov <martyanovmax@mail.ru>
2025-02-25 14:24:59 +0300

    chore(app): add HabitMateApp.kt

--- app/src/main/java/max/keils/habitmate/HabitMateApp.kt
+++ app/src/main/java/max/keils/habitmate/HabitMateApp.kt
@@ -1,10 +1,13 @@
 package max.keils.habitmate
 
 import android.app.Application
+import max.keils.habitmate.di.ApplicationComponent
+import max.keils.habitmate.di.ApplicationScope
 import max.keils.habitmate.di.DaggerApplicationComponent
 
+@ApplicationScope
 class HabitMateApp : Application() {
 
-    val component = DaggerApplicationComponent.create()
+    val component: ApplicationComponent = DaggerApplicationComponent.create()
 
 }
